5e2cf3c37f53990ff597f5f50ae5d8fb3ebc9cb0
Alan Yeverino <lanyev@gmail.com>
2025-01-18 00:32:05 -0700

    back to 1st commit again

--- moba_dashboard.py
+++ moba_dashboard.py
@@ -17,7 +17,7 @@ st.set_page_config(
 # Configuración de caché para mejorar el rendimiento
 @st.cache_data
 def load_data():
-    data = pd.read_csv("hots_cleaned_data.csv")
+    data = pd.read_csv("hots_cleaned_data_modified.csv")
     data["GameTime"] = pd.to_timedelta(data["GameTime"], errors="coerce")
     if "Date" in data.columns:
         data["Date"] = pd.to_datetime(data["Date"], errors="coerce")
